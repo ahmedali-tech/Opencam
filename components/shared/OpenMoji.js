55f541ab43e814bd1008cf80b41a03029cef6857
ahmed ali <aniazi.bese20seecs@seecs.edu.pk>
2022-09-15 09:24:28 +0500

    openmojis converted to text

--- components/shared/OpenMoji.js
+++ components/shared/OpenMoji.js
@@ -3,7 +3,24 @@ import SVG from "react-inlinesvg";
 
 class OpenMoji extends Component {
   render() {
-    return <span>{this.props.label}</span>;
+    if (this.props.hexcode === "1F6F5") {
+      return <span>Bike</span>;
+    }
+    if (this.props.hexcode === "1F6F5") {
+      return <span>Truck</span>;
+    }
+    if (this.props.hexcode === "1F697") {
+      return <span>car</span>;
+    }
+    if (this.props.hexcode === "1F68C") {
+      return <span>Bus</span>;
+    }
+    if (this.props.hexcode === "1F6B2") {
+      return <span>Cycle</span>;
+    }
+    if (this.props.hexcode === "1F6B2") {
+      return <span>Person</span>;
+    }
   }
 }
 
