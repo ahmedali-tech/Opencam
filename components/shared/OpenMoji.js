5525ff3fc889c0088fbd9c0ad8d3b1a4bd9e388b
ahmed ali <72961986+ahmedali-tech@users.noreply.github.com>
2022-09-19 09:58:56 +0500

    Update OpenMoji.js

--- components/shared/OpenMoji.js
+++ components/shared/OpenMoji.js
@@ -1,27 +1,19 @@
-import React, { Component } from "react";
-import SVG from "react-inlinesvg";
+import React, { Component } from 'react'
+import SVG from 'react-inlinesvg';
 
 class OpenMoji extends Component {
+
   render() {
-    if (this.props.hexcode === "1F6F5") {
-      return <span>Bike</span>;
-    }
-    if (this.props.hexcode === "1F6F5") {
-      return <span>Truck</span>;
-    }
-    if (this.props.hexcode === "1F697") {
-      return <span>car</span>;
-    }
-    if (this.props.hexcode === "1F68C") {
-      return <span>Bus</span>;
-    }
-    if (this.props.hexcode === "1F6B2") {
-      return <span>Cycle</span>;
-    }
-    if (this.props.hexcode === "1F6B2") {
-      return <span>Person</span>;
-    }
+    return (
+      <SVG 
+        className="svg-openmoji w-10"
+        cacheRequests={true}
+        src={`/static/icons/openmojis/${this.props.hexcode}.svg`}
+        aria-label={this.props.label}
+      />
+    )
   }
 }
 
-export default OpenMoji;
+export default OpenMoji
+
